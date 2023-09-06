ae58f45684e505c9c437f501bca7d88e47896e9d
Tommy Visic <tommy@tommyvisic.com>
2023-09-06 15:12:40 -0500

    Experimental screen-scoped top bar actions
    In non-compose Android it was common for Fragments to contribute actions to the top level app bar. This change is an experiment to accomplish the same with Compose. It works, but I don't know if it's done in a best practice manner yet.

--- app/src/main/java/com/tommyvisic/spaceflightnews/MainActivity.kt
+++ app/src/main/java/com/tommyvisic/spaceflightnews/MainActivity.kt
@@ -3,33 +3,42 @@ package com.tommyvisic.spaceflightnews
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import com.tommyvisic.spaceflightnews.feed.presentation.FeedScreen
 import com.tommyvisic.spaceflightnews.ui.theme.SpaceFlightNewsTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             SpaceFlightNewsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    FeedScreen()
+                val topBarActionsState = remember { mutableStateOf<@Composable RowScope.() -> Unit>({ }) }
+
+                Scaffold(
+                    topBar = {
+                        CenterAlignedTopAppBar(
+                            title = { Text(text = stringResource(id = R.string.top_bar_title)) },
+                            actions = topBarActionsState.value
+                        )
+                    }
+                ) { innerPadding ->
+                    FeedScreen(
+                        topBarActionsState,
+                        Modifier.padding(innerPadding)
+                    )
                 }
             }
         }
--- app/src/main/java/com/tommyvisic/spaceflightnews/feed/presentation/FeedScreen.kt
+++ app/src/main/java/com/tommyvisic/spaceflightnews/feed/presentation/FeedScreen.kt
@@ -2,7 +2,6 @@ package com.tommyvisic.spaceflightnews.feed.presentation
 
 import android.content.Context
 import android.net.Uri
-import android.util.Log
 import androidx.browser.customtabs.CustomTabsIntent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
@@ -10,6 +9,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -28,14 +28,13 @@ import androidx.compose.material.icons.rounded.Refresh
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -43,26 +42,20 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalInspectionMode
-import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.repeatOnLifecycle
 import androidx.paging.LoadState
-import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.paging.compose.itemContentType
 import androidx.paging.compose.itemKey
 import coil.compose.AsyncImagePainter
 import coil.compose.rememberAsyncImagePainter
 import com.tommyvisic.spaceflightnews.R
-import kotlinx.coroutines.flow.collect
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FeedScreen(
+    topBarActionsState: MutableState<@Composable RowScope.() -> Unit >,
     modifier: Modifier = Modifier,
     viewModel: FeedViewModel = hiltViewModel(),
 ) {
@@ -79,138 +72,131 @@ fun FeedScreen(
         }
     }
 
-    // This column arranges the top app bar plus the articles list.
-    Column(Modifier.fillMaxSize()) {
-        // This is a single screen app so I'm just putting the top app bar here so I can easily get
-        // the click event from the refresh button.
-        CenterAlignedTopAppBar(
-            title = { Text(text = stringResource(id = R.string.top_bar_title)) },
-            actions = {
-                IconButton(onClick = { viewModel.onRefresh() }) {
-                    Icon(
-                        imageVector = Icons.Rounded.Refresh,
-                        contentDescription = stringResource(id = R.string.refresh)
-                    )
+    // Set the actions we'd like to see for this screen.
+    LaunchedEffect(Unit) {
+        topBarActionsState.value = {
+            IconButton(onClick = { viewModel.onRefresh() }) {
+                Icon(
+                    imageVector = Icons.Rounded.Refresh,
+                    contentDescription = stringResource(id = R.string.refresh)
+                )
+            }
+        }
+    }
+
+    // Our main list of articles. Most of the UI for this screen is driven by this list. In
+    // addition to showing the articles themselves, the list also shows various status messages
+    // as items in the list. For example "loading articles" and "error getting articles".
+    LazyColumn(
+        contentPadding = PaddingValues(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        modifier = modifier.fillMaxSize()
+    ) {
+        // Check whether we're refreshing before emitting article items. Don't show any items
+        // while a refresh is underway.
+        when (loadState?.refresh) {
+            // If we're not loading, show the article items.
+            is LoadState.NotLoading -> {
+                items(
+                    count = articles.itemCount,
+                    key = articles.itemKey { it.id },
+                    contentType = articles.itemContentType { "article" }
+                ) {index ->
+                    val item = articles[index]
+                    item?.let { article ->
+                        ArticleCard(
+                            article = article,
+                            onClick = { launchCustomTab(context, article.url) }
+                        )
+                    }
                 }
             }
-        )
-
-        // Our main list of articles. Most of the UI for this screen is driven by this list. In
-        // addition to showing the articles themselves, the list also shows various status messages
-        // as items in the list. For example "loading articles" and "error getting articles".
-        LazyColumn(
-            contentPadding = PaddingValues(16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(16.dp),
-            modifier = modifier.fillMaxSize()
-        ) {
-
-
-            // Check whether we're refreshing before emitting article items. Don't show any items
-            // while a refresh is underway.
-            when (loadState?.refresh) {
-                // If we're not loading, show the article items.
-                is LoadState.NotLoading -> {
-                    items(
-                        count = articles.itemCount,
-                        key = articles.itemKey { it.id },
-                        contentType = articles.itemContentType { "article" }
-                    ) {index ->
-                        val item = articles[index]
-                        item?.let { article ->
-                            ArticleCard(
-                                article = article,
-                                onClick = { launchCustomTab(context, article.url) }
-                            )
-                        }
+            // If we are loading, show a loading indicator.
+            LoadState.Loading -> {
+                item {
+                    Column(
+                        modifier = Modifier.fillParentMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Text(
+                            text = stringResource(id = R.string.fetching_articles),
+                            style = MaterialTheme.typography.headlineSmall
+                        )
+                        Spacer(modifier = Modifier.height(16.dp))
+                        CircularProgressIndicator()
                     }
                 }
-                // If we are loading, show a loading indicator.
-                LoadState.Loading -> {
-                    item {
-                        Column(
-                            modifier = Modifier.fillParentMaxSize(),
-                            horizontalAlignment = Alignment.CenterHorizontally,
-                            verticalArrangement = Arrangement.Center
-                        ) {
-                            Text(
-                                text = stringResource(id = R.string.fetching_articles),
-                                style = MaterialTheme.typography.headlineSmall
-                            )
-                            Spacer(modifier = Modifier.height(16.dp))
-                            CircularProgressIndicator()
-                        }
-                    }
-                }
-                // If there's been an error, show a retry button.
-                is LoadState.Error -> {
-                    item {
-                        Column(
-                            modifier = Modifier.fillParentMaxSize(),
-                            horizontalAlignment = Alignment.CenterHorizontally,
-                            verticalArrangement = Arrangement.Center
-                        ) {
-                            Text(
-                                text = stringResource(id = R.string.error_getting_articles),
-                                style = MaterialTheme.typography.headlineSmall
-                            )
-                            Spacer(modifier = Modifier.height(16.dp))
-                            Button(
-                                onClick = { articles.refresh() }
-                            ) {
-                                Text(text = stringResource(R.string.try_again))
-                            }
-                        }
-                    }
-                }
-                else -> Unit
             }
-
-            // Emit UI based on the append load state. This involves adding an item to the end of
-            // list to indicate status. You might not ever see this in the UI if the network calls
-            // in the data layer are speedy (there's a delay call you can uncomment in
-            // ArticlesRemoteMediator to simulate a slow connection).
-            when (loadState?.append) {
-                // We reached the end of the articles list and want to show a loading indicator.
-                LoadState.Loading -> {
-                    item {
-                        Column(
-                            modifier = Modifier.padding(16.dp),
-                            horizontalAlignment = Alignment.CenterHorizontally,
-                            verticalArrangement = Arrangement.Center
+            // If there's been an error, show a retry button.
+            is LoadState.Error -> {
+                item {
+                    Column(
+                        modifier = Modifier.fillParentMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Text(
+                            text = stringResource(id = R.string.error_getting_articles),
+                            style = MaterialTheme.typography.headlineSmall
+                        )
+                        Spacer(modifier = Modifier.height(16.dp))
+                        Button(
+                            onClick = { articles.refresh() }
                         ) {
-                            Text(
-                                text = stringResource(id = R.string.fetching_articles),
-                                style = MaterialTheme.typography.headlineSmall
-                            )
-                            Spacer(modifier = Modifier.height(16.dp))
-                            CircularProgressIndicator()
+                            Text(text = stringResource(R.string.try_again))
                         }
                     }
                 }
-                // We tried to load more items but failed so show an error message and retry button.
-                is LoadState.Error -> {
-                    item {
-                        Column(
-                            modifier = Modifier.padding(16.dp),
-                            horizontalAlignment = Alignment.CenterHorizontally,
-                            verticalArrangement = Arrangement.Center
-                        ) {
-                            Text(
-                                text = stringResource(id = R.string.error_getting_articles),
-                                style = MaterialTheme.typography.headlineSmall
-                            )
-                            Spacer(modifier = Modifier.height(16.dp))
-                            Button(
-                                onClick = { articles.retry() }
-                            ) {
-                                Text(text = stringResource(R.string.try_again))
-                            }
-                        }
-                    }
-                }
-                else -> Unit
             }
+            else -> Unit
+        }
+
+        // Emit UI based on the append load state. This involves adding an item to the end of
+        // list to indicate status. You might not ever see this in the UI if the network calls
+        // in the data layer are speedy (there's a delay call you can uncomment in
+        // ArticlesRemoteMediator to simulate a slow connection).
+        when (loadState?.append) {
+            // We reached the end of the articles list and want to show a loading indicator.
+            LoadState.Loading -> {
+                item {
+                    Column(
+                        modifier = Modifier.padding(16.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Text(
+                            text = stringResource(id = R.string.fetching_articles),
+                            style = MaterialTheme.typography.headlineSmall
+                        )
+                        Spacer(modifier = Modifier.height(16.dp))
+                        CircularProgressIndicator()
+                    }
+                }
+            }
+            // We tried to load more items but failed so show an error message and retry button.
+            is LoadState.Error -> {
+                item {
+                    Column(
+                        modifier = Modifier.padding(16.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Text(
+                            text = stringResource(id = R.string.error_getting_articles),
+                            style = MaterialTheme.typography.headlineSmall
+                        )
+                        Spacer(modifier = Modifier.height(16.dp))
+                        Button(
+                            onClick = { articles.retry() }
+                        ) {
+                            Text(text = stringResource(R.string.try_again))
+                        }
+                    }
+                }
+            }
+            else -> Unit
         }
     }
 }
